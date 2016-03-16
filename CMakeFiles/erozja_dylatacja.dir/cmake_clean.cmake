3e691c47cacd0b6aebf89d361d984bab9388eb8c
Wojciech Wais <wojciech.wais@gmail.com>
2016-03-16 23:17:21 +0100

    opencv_erozja_dylatacja

---
+++ CMakeFiles/erozja_dylatacja.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/erozja_dylatacja.dir/erozja_dylatacja.o"
+  "erozja_dylatacja.pdb"
+  "erozja_dylatacja"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/erozja_dylatacja.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
